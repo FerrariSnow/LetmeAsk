f0664581f28c5400a288c717041a5933d134c365
Ferrari <mateuseduferrari18@gmail.com>
2021-08-27 00:04:41 -0300

    initial css fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/global.css'
 import '../styles/auth.css'
 
 export function Home() {
