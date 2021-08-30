68713a777b1b98186b7d18ecc1c0d8a042378984
Ferrari <mateuseduferrari18@gmail.com>
2021-08-30 20:19:34 -0300

    Navegação entre criar sala e home

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link  } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -27,7 +29,7 @@ export function NewRoom() {
             </Button>
           </form>
           <h3>
-            Quer entrar em uma sala existente ? <a href="#">clique aqui.</a>
+            Quer entrar em uma sala existente ? <Link to="/">clique aqui.</Link>
           </h3>
         </div>
       </main>
