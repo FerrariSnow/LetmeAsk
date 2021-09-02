6118480229dfa451a728855d949ec641d494587d
Ferrari <mateuseduferrari18@gmail.com>
2021-09-02 00:19:14 -0300

    salas css 1.0

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/button';
+
+import '../styles/room.scss'
+
 export function Room() {
   return (
-    <h1>#BullsNation</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Let me Ask" />
+          <div className="code">#BullsNation</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala teste</h1>
+          <span>Nº de perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar ?"/>
+        </form>
+
+        <div className="form-footer">
+          <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+          <Button type="submit">Enviar pergunta</Button>
+        </div>
+      </main>
+    </div>
   );
 }
