20839366b440efc3491f46b3445353739a5a9b18
Ferrari <mateuseduferrari18@gmail.com>
2021-09-05 20:14:14 -0300

    Remoção de perguntas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg'
+
 import { Button } from '../components/button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
@@ -22,6 +24,12 @@ export function AdminRoom() {
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if(window.confirm('Tem certeza que você deseja excluir esta pergunta ?')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
 
   return (
@@ -49,7 +57,14 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
-            />
+            >
+              <button
+                type="button"
+                onClick={() => handleDeleteQuestion(question.id)}
+              >
+                <img src={deleteImg} alt="Remover pergunta" />
+              </button>
+            </ Question>
           )
           })}
         </div>
